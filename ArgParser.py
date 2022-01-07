989a8dd82716ce2d835afa97d02c61a6eb697f90
Aristi_Papastavrou <sdi1800154@di.uoa.gr>
2022-01-07 16:32:26 +0200

    Updated arg parser with new parameters

--- ArgParser.py
+++ ArgParser.py
@@ -1,7 +1,7 @@
 import argparse
 
 #question_num = 1,2,3 according to the arguemnt parser we want for each question
-def create_Parser(question_num):
+def create_hyperparameter_parser(question_num):
 
     parser = argparse.ArgumentParser()
     if question_num == 1 or question_num == 2:
@@ -23,6 +23,28 @@ def create_Parser(question_num):
     args = parser.parse_args()
     return args
     
-_args = create_Parser(3)
+def createParser(question_num):
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', dest='dataset_path', action='store', help="enter dataset path")
+    
+    if question_num == 1 or question_num == 2:
+        parser.add_argument('-n', dest='time_series_num', action='store', help="enter number of time series selected")
+        parser.add_argument('-mae', dest='threshold', action='store', help="enter error value as double")
+        #B
+        if question_num == 2:
+            parser.add_argument('-mae', dest='threshold', action='store', help="enter error value as double")
+        
+    if question_num == 3:
+        parser.add_argument('-q', dest='query_set', action='store', help="enter queryset path")
+        parser.add_argument('-od', dest='output_path', action='store', help="enter output_dataset_file")
+        parser.add_argument('-oq', dest='output_query_path', action='store', help="enter output_query_file")
+           
+        
+    args = parser.parse_args()
+    return args
+    
+
+_args = create_hyperparameter_parser(3)
 print(_args._get_args)
 print(_args.epochs)
